0257a127ce24e0b836c2da5eb1f2de8bc96bdffe
Raphael Spencer <raphael.spencer@cellcom.com>
2018-11-11 17:12:27 -0600

    Bundle JS directly in HTML file
    First, we open a file stream, and write a beginning chunk
    of HTML.  Then, we stream the Browserify bundle into a
    <script> tag.  Finally, we stream the ending HTML chunk.

--- build_client.js
+++ build_client.js
@@ -1,5 +1,9 @@
 const browserify = require('browserify');
 const watchify = require('watchify');
+const fs = require('fs');
+
+const client_outfile_path = './build_client/stormwatch_client.html';
+
 
 function bundle_client() {
   const bundler = browserify({
@@ -10,12 +14,47 @@ function bundle_client() {
     plugin: [watchify]
   });
 
-  function log_bundle() {
-    bundler.bundle().pipe(process.stdout);
+  function bundle_to_file() {
+    fs.unlink(client_outfile_path, () => {
+      const outfile = fs.createWriteStream(client_outfile_path);
+      outfile.write(html_begin(), () => {
+        bundler.bundle().pipe(outfile);
+        outfile.on('finish', () => {
+          fs.appendFileSync(client_outfile_path, html_end());
+        });
+      });
+    })
   }
 
-  bundler.on('update', log_bundle);
-  log_bundle();
+  bundler.on('update', bundle_to_file);
+  bundle_to_file();
 };
-
 bundle_client();
+
+
+
+
+
+
+
+function html_begin() {
+  return `<!DOCTYPE html></script>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>StormWatch</h1>
+  <script type="text/javascript">`;
+} 
+
+function html_end() {
+  return `
+    </script>
+  </body>
+  </html>
+`;
+}
